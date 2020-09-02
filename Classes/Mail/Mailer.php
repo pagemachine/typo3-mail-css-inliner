7997d039ca450e103c1b2652c108486ee6b3c7dd
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-02 16:28:54 +0200

    [FEATURE] Process Symfony mail HTML body

---
+++ Classes/Mail/Mailer.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace Pagemachine\MailCssInliner\Mail;
+
+/*
+ * This file is part of the Pagemachine Mail CSS Inliner project.
+ */
+
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\RawMessage;
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+use TYPO3\CMS\Core\Mail\Mailer as CoreMailer;
+
+final class Mailer extends CoreMailer
+{
+    /**
+     * @inheritdoc
+     */
+    public function send(RawMessage $message, Envelope $envelope = null): void
+    {
+        if ($message instanceof Email) {
+            $converter = new CssToInlineStyles();
+            $message->html($converter->convert($message->getHtmlBody()));
+        }
+
+        parent::send($message, $envelope);
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,17 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-call_user_func(function () {
-    /** @var TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-    $signalSlotDispatcher->connect(
-        \TYPO3\CMS\Core\Mail\Mailer::class,
-        'postInitializeMailer',
-        \Pagemachine\MailCssInliner\Slots\MailerSlot::class,
-        'registerPlugin'
-    );
-});
+(function () {
+    if (version_compare(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version(), '10', '>=')) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\Mailer::class]['className'] = \Pagemachine\MailCssInliner\Mail\Mailer::class;
+    } else {
+        /** @var TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
+        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+        $signalSlotDispatcher->connect(
+            \TYPO3\CMS\Core\Mail\Mailer::class,
+            'postInitializeMailer',
+            \Pagemachine\MailCssInliner\Slots\MailerSlot::class,
+            'registerPlugin'
+        );
+    }
+})();
