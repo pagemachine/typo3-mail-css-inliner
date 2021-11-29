622fd6cc2458155c0e568ea8484e2889c3501606
Mathias Brodala <mbrodala@pagemachine.de>
2021-11-29 16:11:11 +0100

    [TASK] Drop Swiftmailer-related code

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,4 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-(function () {
-    if (version_compare(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version(), '10', '>=')) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\Mailer::class]['className'] = \Pagemachine\MailCssInliner\Mail\Mailer::class;
-    } else {
-        /** @var TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
-        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-        $signalSlotDispatcher->connect(
-            \TYPO3\CMS\Core\Mail\Mailer::class,
-            'postInitializeMailer',
-            \Pagemachine\MailCssInliner\Slots\MailerSlot::class,
-            'registerPlugin'
-        );
-    }
-})();
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Mail\Mailer::class]['className'] = \Pagemachine\MailCssInliner\Mail\Mailer::class;
