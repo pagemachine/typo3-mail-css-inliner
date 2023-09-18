3e3104d0e7225609a7627ddf3c31694b482d21bf
Mathias Brodala <mbrodala@pagemachine.de>
2023-09-18 10:35:58 +0200

    [TASK] Mirror "php" dependency to ext_emconf.php
    This is essential for installations in Legacy Mode.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.0',
     'constraints' => [
         'depends' => [
+            'php' => '8.0.0-8.99.99',
             'typo3' => '11.5.0-12.4.99',
         ],
     ],
