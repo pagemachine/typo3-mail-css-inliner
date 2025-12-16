a4847a6212942d69d812302550c823b72314b55f
Mathias Brodala <mbrodala@pagemachine.de>
2025-12-16 11:54:46 +0100

    Revert "Drop ext_emconf"

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 Mail CSS Inliner',
+    'description' => 'Inlines CSS in all outgoing TYPO3 mails.',
+    'category' => 'misc',
+    'author' => 'Mathias Brodala',
+    'author_email' => 'mbrodala@pagemachine.de',
+    'author_company' => 'Pagemachine AG',
+    'state' => 'stable',
+    'version' => '3.1.2',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.0.0-8.99.99',
+            'typo3' => '11.5.0-12.4.99',
+        ],
+    ],
+];
